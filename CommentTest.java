45187924cfc33cc4c84c317aa3500b8c9e4b3aca
Megh-patel-2008 <m3ghpatel2008@gmail.com>
2025-11-10 21:10:46 -0500

    Questions 13 to 18

---
+++ CommentTest.java
@@ -0,0 +1,54 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class CommentTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CommentTest
+{
+    private Comment c1;
+    /**
+     * Default constructor for test class CommentTest
+     */
+    public CommentTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        c1 = new Comment("Megh", "Very nice", 3);
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    public void testAuthorAndRatingStored(){
+        assertEquals("Megh", c1.getAuthor());
+        assertEquals(3, c1.getRating());
+    }
+    
+    public void testCommentNotNull()
+    {
+        assertNotNull(c1);
+    }
+}
